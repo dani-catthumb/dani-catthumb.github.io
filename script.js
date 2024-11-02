47aae84e9b43a1dd406883001892c25dcde131fa
dani-catthumb <bulatov.dani@gmail.com>
2024-11-02 17:23:13 +0200

    working on text in hovertips

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ async function getData() {
     return await response.json();
 }
 
+async function getHovertipJSON() {
+    const response = await fetch('hovertip.json');
+    return await response.json();
+}
+
 async function fetchContent() {
     const data = await getData();
     displayContent(data);
@@ -15,16 +20,22 @@ function createValueDiv(value) {
     return valueDiv;
 }
 
-function createTagDiv(tag) {
-    const tagDiv = document.createElement('div');
-    tagDiv.classList.add("tag-div");
-
+function createTagAbbr(tag, hovertipJSON) {
     const tagAbbr = document.createElement('abbr');
     tagAbbr.textContent = tag;
     tagAbbr.title = tag;
-    
+
+    return tagAbbr
+}
+
+function createTagDiv(tag, hovertipJSON) {
+    const tagDiv = document.createElement('div');
+    tagDiv.classList.add("tag-div", tag);
+
+    const tagAbbr = createTagAbbr(tag, hovertipJSON);
+
     tagDiv.appendChild(tagAbbr);
-    
+
     return tagDiv;
 }
 
@@ -35,12 +46,16 @@ function createContainerDiv(...children) {
     return containerDiv;
 }
 
-function displayContent(content) {
+async function displayContent(content) {
     const mainElement = document.getElementById('content-display');
     mainElement.innerHTML = '';
+    const hovertipJSON = await getHovertipJSON()
 
     content.forEach(item => {
-        const containerDiv = createContainerDiv(createValueDiv(item.value), createTagDiv(item.tag));
+        const containerDiv = createContainerDiv(
+            createValueDiv(item.value),
+            createTagDiv(item.tag, hovertipJSON)
+        );
         mainElement.appendChild(containerDiv);
     });
 }
@@ -56,7 +71,7 @@ async function handleSearchInput() {
         };
         const fuse = new Fuse(data, options);
         const result = fuse.search(inputText);
-        
+
         const fuzzyMatchedData = result.map(res => {
             const item = data.find(item => item[options.keys[0]] === res.item[options.keys[0]]);
             return { ...item };
